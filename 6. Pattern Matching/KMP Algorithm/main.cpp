d04e1e819874e8927a912655f8795c52a438b83a
Daojing <daojing.zhai@gmail.com>
2018-05-02 18:45:24 +0800

    renew

--- 6. Pattern Matching/KMP Algorithm/main.cpp
+++ 6. Pattern Matching/KMP Algorithm/main.cpp
@@ -2,23 +2,28 @@
 //  main.cpp
 //  KMP Algorithm
 //
-//  Created by Daojing Zhai on 2018/5/1.
+//  Created by Daojing Zhai on 2018/5/2.
 //  Copyright © 2018 Daojing Zhai. All rights reserved.
 //
 
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <string>
 using namespace std;
+
+int strlen(char *p){
+    int l=0;
+    while(*p && *p!='\n'){
+              l++;
+              p++;
+          }
+    return l;
+}
 
 const int maxnum = 1000; //max length of pattern.
 int next_index[maxnum];
 
-
 void assignstr(string input, char *target, char *pattern){
-    int lengthstr =input.length();
+    int lengthstr = input.length();
     int pat_index = 0;
     int tar_index = 0;
     int input_index = 0;
@@ -28,14 +33,14 @@ void assignstr(string input, char *target, char *pattern){
         input_index++;
     }
     input_index++;
-    while (input_index<lengthstr) {
+    while (input_index<lengthstr){
         pattern[pat_index]=input[input_index];
         pat_index++;
         input_index++;
     }
 }
 
-void getnext(char *pat, int lengthP){// find next_index[j] of pattern
+void getnext(char *pat, int lengthP){
     int j=0, k=-1;
     next_index[0] = -1;
     while(j<lengthP){
@@ -44,51 +49,60 @@ void getnext(char *pat, int lengthP){// find next_index[j] of pattern
             k++;
             next_index[j] = k;
         }
-        else k=next_index[k];
+        else
+            k=next_index[k];
     }
 }
 
-
 void KMPsearch(char *pat, char *target){
-    getnext(pat, strlen(pat)); // find array next_index[j];
-
+    getnext(pat, strlen(pat));
     int posP = 0, posT = 0;
-    int flag = 0; //indicate is there are any finding.
+    int flag = 0;
     int count = 0;
-    int lengthP = strlen(pat), lengthT = strlen(target);
-    while (posT<lengthT) {
-        if (posP == -1||pat[posP]==target[posT]) {
-            posP++;
-            posT++;
-        }
-        else posP = next_index[posP];
-        if (posP == lengthP) {
-            if (count!=0) {
-                cout<<' ';
+    int lengthP = strlen(pat);
+    int lengthT = strlen(target);
+    if (lengthP == 1){ //单独讨论lengthP=1的情况
+        for (int i=0; i<lengthT; i++) {
+            if (target[i] == pat[0]){
+                if (count!=0) cout<<' ';
+                cout<<i;
+                flag = 1;
+                count++;
             }
-            cout<<posT-lengthP;
-            flag = 1;
-            posP = 0;
-            posT--;
-            count++;
-            
         }
+    }
+    else{ //KMP算法求解lengthP!=1的情况
+        while (posT<lengthT && posP<lengthP){
+            if (posP == -1||pat[posP]==target[posT]){
+                posP++;
+                posT++;
+            }
+            else posP = next_index[posP];
+            if (posP == lengthP){
+                if (count!=0)
+                    cout<<' ';
+                cout<<posT-lengthP;
+                flag = 1;
+                posP = 0;
+                posT = posT-lengthP+1;
+                count++;
+            }
+        }
+        
     }
     if (flag==1) cout<<endl;
     if (flag==0) cout<<"false"<<endl;
 }
 
-
-
 int main(){
     string inputstr;
-    while (getline(cin,inputstr)) {
+    while (getline(cin,inputstr)){
         char *target = new char[maxnum];
         char *pattern = new char[maxnum];
         assignstr(inputstr, target, pattern);
         KMPsearch(pattern, target);
-        delete []target; //free dynamic array
-        delete []pattern; //free dynamic array
+        delete []target;
+        delete []pattern;
     }
     return 0;
 }
